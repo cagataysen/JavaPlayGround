c6a80de80f2215f00f1a15fb12a2143dea5f33ea
Cagatay Sen <orhancagataysen@outlook.com>
2024-11-20 15:03:53 +0100

    java oop concepts practised

---
+++ src/main/java/org/CoreJavaPlayGround/Main.java
@@ -0,0 +1,26 @@
+package org.CoreJavaPlayGround;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    public @interface cagatayAnnotation{
+        String value();
+        int priority() default 1;
+    }
+
+    @cagatayAnnotation(value = "example")
+    public void exampleMethod(){
+        System.out.println("Hello world");
+    }
+
+    Class
+}
